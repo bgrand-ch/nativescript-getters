9e87bf772faf88cfe6f82e9c6b21744e279f5c53
Benjamin Grand <17108974+elvticc@users.noreply.github.com>
2019-10-29 21:10:58 +0100

    Added Vue demo

---
+++ demo-vue/app/components/Home.vue
@@ -0,0 +1,71 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar">
+            <Label class="action-bar-title" text="Home"></Label>
+        </ActionBar>
+
+        <StackLayout id="main">
+            <!-- Add your page content here -->
+
+            <Label text="First name:"></Label>
+            <TextField hint="First name..." class="bold"></TextField>
+
+            <Label text="Last name:"></Label>
+            <TextField hint="Last name..." class="italic"></TextField>
+
+            <Label text="Description:"></Label>
+            <TextView hint="Description..."></TextView>
+
+            <StackLayout>
+
+                <Label text="Address:"></Label>
+                <TextView hint="Address..."></TextView>
+
+                <Label text="Website:"></Label>
+                <TextField hint="Website..." keyboardType="url"></TextField>
+
+                <StackLayout id="contact">
+
+                    <Label text="Email:"></Label>
+                    <TextField hint="Email..." keyboardType="email"></TextField>
+
+                    <Label text="Phone number:"></Label>
+                    <TextField hint="Phone number..." keyboardType="phone"></TextField>
+
+                </StackLayout>
+
+            </StackLayout>
+
+            <Label text="Password:"></Label>
+            <TextField hint="Password..." secure="true"></TextField>
+
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+    import "nativescript-getters";
+
+    export default {
+        computed: {
+            message() {
+                return "Blank {N}-Vue app";
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    // Start custom common variables
+    @import '../app-variables';
+    // End custom common variables
+
+    // Custom styles
+    .fa {
+        color: $accent-dark;
+    }
+
+    .info {
+        font-size: 20;
+    }
+</style>
