81f61d99d67698f3ca867a894df86d0d48beb1fa
Benjamin Grand <17108974+bgrand-ch@users.noreply.github.com>
2021-04-16 13:20:31 +0200

    fix: remove TabNavigationBase

--- src/getters.ts
+++ src/getters.ts
@@ -1,9 +1,8 @@
 import {
-    Frame,
-    LayoutBase,
-    Page,
-    TabNavigationBase,
-    TextBase
+  Frame,
+  LayoutBase,
+  Page,
+  TextBase
 } from "@nativescript/core";
 
 import { getViewsByTag, getViewsByTags } from "./methods/tags.method";
@@ -13,105 +12,94 @@ import { getViewsByProperty, getViewsByProperties } from "./methods/properties.m
 import { getViewsByValuePair, getViewsByValuePairs } from "./methods/value-pairs.method";
 import { getViewByIdentifier, getViewsByIdentifiers } from "./methods/identifiers.method";
 
-const nsClasses: Array<Frame|LayoutBase|Page|TabNavigationBase|TextBase> = [
-
-    Frame.prototype,
-    LayoutBase.prototype,
-    Page.prototype,
-    TabNavigationBase.prototype,
-    TextBase.prototype
-
+const nsClasses: Array<Frame|LayoutBase|Page|TextBase> = [
+  Frame.prototype,
+  LayoutBase.prototype,
+  Page.prototype,
+  TextBase.prototype
 ];
 const newMethods = {
+  // Tags
+  "getViewsByTag": {
+      "value": getViewsByTag
+  },
+  "getViewsByTags": {
+      "value": getViewsByTags
+  },
 
-    // Tags
-    "getViewsByTag": {
-        "value": getViewsByTag
-    },
-    "getViewsByTags": {
-        "value": getViewsByTags
-    },
+  // Types
+  "getViewsByType": {
+      "value": getViewsByType
+  },
+  "getViewsByTypes": {
+      "value": getViewsByTypes
+  },
 
-    // Types
-    "getViewsByType": {
-        "value": getViewsByType
-    },
-    "getViewsByTypes": {
-        "value": getViewsByTypes
-    },
+  // Classes
+  "getViewsByClass": {
+      "value": getViewsByClass
+  },
+  "getViewsByClasses": {
+      "value": getViewsByClasses
+  },
 
-    // Classes
-    "getViewsByClass": {
-        "value": getViewsByClass
-    },
-    "getViewsByClasses": {
-        "value": getViewsByClasses
-    },
+  // Properties
+  "getViewsByProperty": {
+      "value": getViewsByProperty
+  },
+  "getViewsByProp": { // alias
+      "value": getViewsByProperty
+  },
+  "getViewsByAttribute": { // alias
+      "value": getViewsByProperty
+  },
+  "getViewsByAttr": { // alias
+      "value": getViewsByProperty
+  },
+  "getViewsByProperties": {
+      "value": getViewsByProperties
+  },
+  "getViewsByProps": { // alias
+      "value": getViewsByProperties
+  },
+  "getViewsByAttributes": { // alias
+      "value": getViewsByProperties
+  },
+  "getViewsByAttrs": { // alias
+      "value": getViewsByProperties
+  },
 
-    // Properties
-    "getViewsByProperty": {
-        "value": getViewsByProperty
-    },
-    "getViewsByProp": { // alias
-        "value": getViewsByProperty
-    },
-    "getViewsByAttribute": { // alias
-        "value": getViewsByProperty
-    },
-    "getViewsByAttr": { // alias
-        "value": getViewsByProperty
-    },
-    "getViewsByProperties": {
-        "value": getViewsByProperties
-    },
-    "getViewsByProps": { // alias
-        "value": getViewsByProperties
-    },
-    "getViewsByAttributes": { // alias
-        "value": getViewsByProperties
-    },
-    "getViewsByAttrs": { // alias
-        "value": getViewsByProperties
-    },
+  // Value pairs
+  "getViewsByValuePair": {
+      "value": getViewsByValuePair
+  },
+  "getViewsByValPair": { // alias
+      "value": getViewsByValuePair
+  },
+  "getViewsByValuePairs": {
+      "value": getViewsByValuePairs
+  },
+  "getViewsByValPairs": { // alias
+      "value": getViewsByValuePairs
+  },
 
-    // Value pairs
-    "getViewsByValuePair": {
-        "value": getViewsByValuePair
-    },
-    "getViewsByValPair": { // alias
-        "value": getViewsByValuePair
-    },
-    "getViewsByValuePairs": {
-        "value": getViewsByValuePairs
-    },
-    "getViewsByValPairs": { // alias
-        "value": getViewsByValuePairs
-    },
-
-    // Identifiers
-    "getViewByIdentifier": {
-        "value": getViewByIdentifier
-    },
-    "getViewsByIdentifiers": {
-        "value": getViewsByIdentifiers
-    }
-
+  // Identifiers
+  "getViewByIdentifier": {
+      "value": getViewByIdentifier
+  },
+  "getViewsByIdentifiers": {
+      "value": getViewsByIdentifiers
+  }
 };
 
 nsClasses.forEach((nsClass) => {
-
-    try {
-
-        Object.defineProperties(nsClass, newMethods);
-
-    } catch (error) {
-
-        console.warn(
-            "-#########-- NATIVESCRIPT GETTERS PLUGIN WARNING --#########-",
-            `NativeScript class: ${nsClass} //`,
-            `Error ${error.name}: ${error.message}`
-        );
-
-    }
-
+  try {
+    Object.defineProperties(nsClass, newMethods);
+  } catch (error) {
+    console.warn(
+      "-#########-- NATIVESCRIPT GETTERS PLUGIN WARNING --#########-",
+      `NativeScript class: ${nsClass} //`,
+      `Error ${error.name}: ${error.message}`
+    );
+  }
 });
--- src/typings/extended-tabs.d.ts
+++ src/typings/extended-tabs.d.ts
@@ -1,11 +1,11 @@
-import {
-    TabNavigationBase
-} from "@nativescript/core";
+// import {
+//     TabNavigationBase
+// } from "@nativescript/core";
 
-import { IGettersMethods } from "../models/getters-methods.model";
+// import { IGettersMethods } from "../models/getters-methods.model";
 
-declare module "@nativescript/core/ui/tab-navigation-base/tab-navigation-base/tab-navigation-base" { // before: declare module "tns-core-modules/ui/tab-navigation-base/tab-navigation-base/tab-navigation-base"
+// declare module "@nativescript/core/ui/tab-navigation-base/tab-navigation-base/tab-navigation-base" { // before: declare module "tns-core-modules/ui/tab-navigation-base/tab-navigation-base/tab-navigation-base"
 
-    interface TabNavigationBase extends IGettersMethods {}
+//     interface TabNavigationBase extends IGettersMethods {}
 
-}
+// }
